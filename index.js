3874472ffa37a1c9efd5666f15b31465d31bec70
Ali Reda <aliredamis@gmail.com>
2019-06-15 19:18:37 +0200

    created new route

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-const Hapi    = require('@hapi/hapi'),
-      Request = require('request'),
-      Cheerio = require('cheerio');
+    const Hapi      = require('@hapi/hapi'),
+    ScrapingHandler = require('./ScrapingHandler');
 
-const init = async () => {
+    const init = async () => {
 
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
+        const server = Hapi.server({
+            port: 3000,
+            host: 'localhost'
+        });
+
+        server.route({
+            method: 'GET',
+            path:'/',
+            handler: (request, h) => (
+                ScrapingHandler.getFootballNews()
+            )
+
+        });
+
+        await server.start();
+        console.log('Server running on %s', server.info.uri);
+    };
+
+    process.on('unhandledRejection', (err) => {
+        console.log(err);
+        process.exit(1);
     });
 
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
-};
-
-process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
-});
-
-init();
+    init();
